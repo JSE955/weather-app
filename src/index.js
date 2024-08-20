f30d524c3b4d4a47b832cbcbc4b187b45eab2d1a
JSE955 <jordanscottedwards@gmail.com>
2024-08-20 16:29:04 -0400

    Add function to get weather

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import "./styles.css";
 
-console.log("Testing...");
+const weatherURL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
+const key = 'A2NS9VYU4AG72XUKCQTFPS8NK';
+
+function getWeather(location) {
+  let url = `${weatherURL}${location}?key=${key}`;
+  fetch(url, {mode: 'cors'})
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      console.log(response);
+    });
+}
+
+getWeather('Hanahan');
